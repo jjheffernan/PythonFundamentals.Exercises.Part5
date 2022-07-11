36fdcad186d63cb50210a55821acb82384b9b903
jjheffernan <jjheffernan99@gmail.com>
2022-07-11 01:39:18 -0400

    added length, first/last char, and has substring functionality.

--- string_utils.py
+++ string_utils.py
@@ -3,28 +3,32 @@ def str_len(str_in: str) -> str:
     """
     Given a string parameter, this function should return the length of the parameter.
     """
-    pass  # remove pass statement and implement me
+    return len(str_in)
+    # pass  # remove pass statement and implement me
 
 
 def first_char(str_in: str) -> str:
     """
     Given a string parameter, this function should return the first letter of the parameter.
     """
-    pass  # remove pass statement and implement me
+    return str_in[0]
+    # pass  # remove pass statement and implement me
 
 
 def last_char(str_in: str) -> str:
     """
     Given a string parameter, this function should return the last letter of the parameter..
     """
-    pass  # remove pass statement and implement me
+    return str_in[-1]
+    # pass  # remove pass statement and implement me
 
 
 def input_has_substring(str_in: str, sub_str_in: str) -> bool:
     """
     This function determines if the substring exists within the string. Returns True or False.
     """
-    pass  # remove pass statement and implement me
+    return str_in.__contains__(sub_str_in)
+    # pass  # remove pass statement and implement me
 
 
 def substring(str_in: str, start: int, stop: int) -> str:
@@ -36,7 +40,8 @@ def substring(str_in: str, start: int, stop: int) -> str:
     start -- starting position of the input parameter to start the substring (inclusive)
     stop -- stopping position of the input parameter to stop the substring (exclusive)
     """
-    pass  # remove pass statement and implement me
+
+    # pass  # remove pass statement and implement me
 
 
 def opposite_case(str_in: str) -> str:
